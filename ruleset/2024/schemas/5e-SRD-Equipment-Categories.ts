902b7087e15932deaf78b3bc6641fccb03f33d84
blackjackmh <blackjackok@gmail.com>
2026-04-18 17:16:27 +0300

    Add official 2024 D&D SRD 5.2.1 markdown ruleset (spells, monsters, classes, conditions, feats, equipment)

--- ruleset/2024/schemas/5e-SRD-Equipment-Categories.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import { APIReferenceSchema } from '../../schemas/common';
-
-export const EquipmentCategorySchema = z.object({
-  index: z.string(),
-  name: z.string(),
-  equipment: z.array(APIReferenceSchema),
-  url: z.string(),
-});
